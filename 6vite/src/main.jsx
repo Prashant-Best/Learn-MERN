bb99968f18a6762587ede72a65e3c752f52e753d
Prashant-Best <prashantpanwar003003@gmail.com>
2026-07-09 10:58:59 +0530

    adeed practice file insidde vite folder

--- 6vite/src/main.jsx
+++ 6vite/src/main.jsx
@@ -1,12 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Form from './form.jsx'
-import App from './App.jsx'
+//import Form from './form.jsx'
+//import App from './App.jsx'
+import Prac from './prac.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-    <Form />
+  
+    <Prac/>
+
   </StrictMode>,
 )
